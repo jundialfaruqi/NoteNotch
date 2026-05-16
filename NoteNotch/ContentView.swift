5a28fc9795fcd5d48acdb8918f409c92dd466cdc
jundialfaruqi <jundialfaruqi@gmail.com>
2026-05-16 20:11:10 +0700

    Initial Commit

---
+++ NoteNotch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NoteNotch
+//
+//  Created by Mac Pro m4 on 16/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NoteNotch/NoteNotchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteNotchApp.swift
+//  NoteNotch
+//
+//  Created by Mac Pro m4 on 16/05/26.
+//
+
+import SwiftUI
+
+@main
+struct NoteNotchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
